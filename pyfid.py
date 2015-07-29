cfd956e07969f27862a08564b9bdabab02ddefa3
David Kant <david.kant@gmail.com>
2015-07-29 16:00:52 -0700

    normalize amp confidence, bounce frame to frame

--- pyfid.py
+++ pyfid.py
@@ -20,17 +20,20 @@ def freq_to_midi(f):
 def midi_to_freq(m): 
   return np.power(2, (m-69.0)/12.0) * 440.0 if m!= 0.0 else 0.0
 
-# def bin_to_freq(b): return b * float(sr) / float(n_fft)
-# def freq_to_bin(f): return int(round(f/(float(sr)/float(n_fft)))
-# set params internall to pyfid and call from there?
+def bin_to_freq(b, sr, n_fft):
+  return b * float(sr) / float(n_fft)
+
+def freq_to_bin(f, sr, n_fft): 
+  return np.round(f/(float(sr)/float(n_fft))).astype('int')
 
 # --------------------------------------------------------------------------- #
 # ppitch
 # --------------------------------------------------------------------------- #
 
 def ppitch(y, sr=44100, n_fft=4096, win_length=1024, hop_length=2048, 
-  num_peaks=20, num_pitches=3, min_bin=2, max_bin=743, min_freq=55.0, 
-  max_freq=1000.0):
+  num_peaks=20, num_pitches=3, min_peak=2, max_peak=743, min_fund=55.0, 
+  max_fund=1000.0, harm_offset=0, harm_rolloff=0.75, ml_width=25, 
+  bounce_width=0, max_harm=32767):
  
   """Polyphonic pitch estimation.
 
@@ -41,10 +44,10 @@ def ppitch(y, sr=44100, n_fft=4096, win_length=1024, hop_length=2048,
     - hop_length
     - num_peaks
     - num_pitches
-    - min_bin: min_bin for peak picking
-    - max_bin: max_bin for peak picking
-    - min_freq: min fundamental freq
-    - max_freq: max fundamental freq
+    - min_peak: min_bin for peak picking
+    - max_peak: max_bin for peak picking
+    - min_fund: min fundamental freq
+    - max_fund: max fundamental freq
 
   :returns:
     - pitches
@@ -53,6 +56,10 @@ def ppitch(y, sr=44100, n_fft=4096, win_length=1024, hop_length=2048,
 
   :todo:
     -> min/max_bin should be in freqs so invariant to fft size
+    -> stats for how much bounced
+
+  :notes:
+    - previously was nearest_multiple+1 # to avoid divide by 0
 
    """
 
@@ -71,9 +78,10 @@ def ppitch(y, sr=44100, n_fft=4096, win_length=1024, hop_length=2048,
 
   peak_thresh = 1e-3
 
-  # bins (freq range) to search [make sure we have room either side]
-  #min_bin = min_bin # \\--> make sure there's room below (for peak searching)
-  #max_bin = max_bin # \\--> make sure there's room above (for peak searching)
+  # bins (freq range) to search 
+  min_bin = 2
+  max_bin = freq_to_bin(float(max_peak), sr, n_fft)
+  # //--> [make sure we have room either side for peak picking]
 
   # things we know
   num_bins, num_frames = if_gram.shape
@@ -91,6 +99,7 @@ def ppitch(y, sr=44100, n_fft=4096, win_length=1024, hop_length=2048,
     frqs = if_gram[:,i]
     mags = np.abs(D[:,i])
     total_power = mags.sum()
+    max_amp = np.max(mags)
 
     # neighbor bins
     lower  = mags[(min_bin-1):(max_bin)]
@@ -121,8 +130,8 @@ def ppitch(y, sr=44100, n_fft=4096, win_length=1024, hop_length=2048,
     # ----------------------------------------------------------------------- #
 
     # range we'll consider for fundamentals
-    #min_freq = min_freq
-    #max_freq = max_freq
+    min_freq = float(min_fund)
+    max_freq = float(max_fund)
 
     # from min_bin to max_bin in 48ths of an octave (1/4 tones)
 
@@ -141,18 +150,22 @@ def ppitch(y, sr=44100, n_fft=4096, win_length=1024, hop_length=2048,
     # likelihood function for each bin frequency
     def ml_a(amp): 
       """a factor depending on the amplitude of the ith peak"""
-      return np.sqrt(np.sqrt(amp))
+#      return np.sqrt(np.sqrt(amp))
+      return np.sqrt(np.sqrt(amp/max_amp))
+      # return np.ones_like(amp)
+      # return amp
 
     def ml_t(freq_ratio):
       """how closely the ith peak is tuned to a multiple of f"""
-      max_dist = 25 # cents
+      max_dist = ml_width # cents
       cents = np.abs(ratio_to_cents(freq_ratio))
-      dist = np.clip(1.0 - (cents / 25), 0, 1)
+      dist = np.clip(1.0 - (cents / max_dist), 0, 1)
       return dist
 
     def ml_i(nearest_multiple): 
       """whether the peak is closest to a high or low multiple of f"""
-      return 1/np.power(nearest_multiple+1, 1) # FUCKING: control param here
+      return 1/np.power(np.clip(nearest_multiple + harm_offset, 1, 32767), harm_rolloff) * (nearest_multiple <= max_harm) 
+      # return 1/np.power(nearest_multiple+1, 2)
       # return np.ones_like(nearest_multiple)
 
     ml = (ml_a(mags_tile) * \
@@ -163,10 +176,40 @@ def ppitch(y, sr=44100, n_fft=4096, win_length=1024, hop_length=2048,
       ml_t((frqs_tile/histo) / (frqs_tile/histo).round()) * \
       ml_i((frqs_tile/histo).round()))
 
+    """super rough hack but work!
+
+    bring this in as a control
+    only tests in one direction (new is higher), adjust so ratio goes both ways
+    what about which harms we're testing for should that be variable too
+    """
+    num_found = 0
+    maybe = 0
+    found_so_far = []
+    prev_frame = list(pitches[i-1]) if i>0 else []
+    indices = ml.argsort()[::-1]
+    while num_found < num_pitches and maybe <= ml.shape[0]:
+      this_one = b2f(indices[maybe])
+      bounce1 = any([any([abs(ratio_to_cents(this_one/(other_one*harm))) < bounce_width for harm in [1,2,3,4,5,6]]) for other_one in found_so_far+prev_frame])
+      bounce2 = any([any([abs(ratio_to_cents(other_one/(this_one*harm))) < bounce_width for harm in [1,2,3,4,5,6]]) for other_one in found_so_far+prev_frame])
+      bounce = bounce1 or bounce2
+      # print this_one, bounce, found_so_far
+      if not bounce:
+        #print 'notbounce'
+        found_so_far += [this_one]
+        num_found += 1
+      # if bounce: print 'bounce!!!!'
+      maybe += 1
+
     indices = ml.argsort()[::-1][0:num_pitches]
-    pitches[i] = b2f(indices)
+    pitches[i] = np.array(found_so_far)
     confidences[i] = ml[indices]
     peaks[i] = peaks_frqs
+
+
+    # indices = ml.argsort()[::-1][0:num_pitches]
+    # pitches[i] = b2f(indices)
+    # confidences[i] = ml[indices]
+    # peaks[i] = peaks_frqs
 
     # ----------------------------------------------------------------------- #
     # estimate fundamental (least squares)
@@ -252,3 +295,16 @@ def ppitch(y, sr=44100, n_fft=4096, win_length=1024, hop_length=2048,
 
   return fundamentals, pitches, D, peaks, confidences, tracks
 
+
+# --------------------------------------------------------------------------- #
+# extras
+# --------------------------------------------------------------------------- #
+
+def pitch_onsets(funds, win=2, depth=25):
+  
+  onsets = np.zeros_like(funds)
+  for i in range(funds.shape[0]):
+    for j in range(funds.shape[1]):
+      onsets[i,j] = (np.abs(pyfid.ratio_to_cents(funds[i-win:i,:] / funds[i,j])) <= thresh).any(axis=1).all()
+
+  return onsets
